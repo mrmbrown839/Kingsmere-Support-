984a4393e6eb3598543182774d2ae09eacced5f0
mrmbrown839 <hurlowjayden86@gmail.com>
2026-06-22 17:35:40 +0100

    Fix typo in bot activity name

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made b Kingsmere Primary School!",
+        name: "Made by Kingsmere Primary School!",
         // Activity type number (0 = Playing).
         type: 0,
       },
